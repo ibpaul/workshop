3ac96c9ca2d5872e123f24065043efbc7b10fc63
Paul Lucas <paullucas82@gmail.com>
2022-09-29 16:25:40 -0500

    created placeholder for play tool

--- tools/play/main.cpp
+++ tools/play/main.cpp
@@ -2,15 +2,8 @@
 using namespace cimg_library;
 
 
-int test_func2()
+int main()
 {
-    return 44;
-}
-
-void load_image()
-{
-    // TODO: Error handling if file not found or unable to be loaded.
-    // TODO: Add PNG support by linking to libpng.
     CImg<unsigned char> image("data/peppers.png");
 
     CImgDisplay main_disp(image, "Test Image");
