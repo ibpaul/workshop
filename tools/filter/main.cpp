9a9f2789ae2f232f68e0d7df8f1a1512b8c46a00
Paul Lucas <paullucas82@gmail.com>
2022-09-30 09:02:17 -0500

    cleaned up how image format is handled in main()

--- tools/filter/main.cpp
+++ tools/filter/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <functional>
+#include <memory>
 
 // Hide error message reporting from CImg.
 #define cimg_verbosity 0
@@ -20,46 +21,47 @@ constexpr size_t NumOfRuns = 25;
 constexpr size_t NumOfRuns = 100;
 #endif
 
+using Image = CImg<unsigned char>;
+
 
 int main(int argc, char* argv[])
 {
     auto opts = process_all_options(argc, argv);
 
+    unique_ptr<Image> image;
     try {
-        CImg<unsigned char> image(opts.input_file.c_str());
-        CImg<unsigned char> output(image.width(), image.height(), image.depth(), image.spectrum());
-
-        //unique_ptr<LTS::util::PerformanceTest> pt;
-
-        LTS::util::PerformanceTest pt {NumOfRuns, opts.report_file};
-
-        LTS::filters::GaussianKernel filter;
-
-        function<void()> test_func = ([&](){
-            filter.process(image.data(), image.width(), image.height(), output.data(), image.spectrum());
-        });
-
-
-
-        #if MULTI_THREADED
-        vector<function<void()>> test_funcs;
-        test_funcs.push_back(test_func);
-        test_funcs.push_back(test_func);
-        test_funcs.push_back(test_func);
-        test_funcs.push_back(test_func);
-        pt.test_threaded(test_funcs);
-        #else
-        pt.test(test_func);
-        #endif
-
-        if (!opts.output_file.empty()) {
-            output.save(opts.output_file.c_str());
-        }
-
+        image = make_unique<Image>(opts.input_file.c_str());
     } catch (...) {
         cerr << "Unable to open file '" << opts.input_file << "'." << endl;
-        return 1;
+        exit(1);
     }
 
-    return 0;
+    Image output(image->width(), image->height(), image->depth(), image->spectrum());
+
+    //unique_ptr<LTS::util::PerformanceTest> pt;
+
+    LTS::util::PerformanceTest pt {NumOfRuns, opts.report_file};
+
+    LTS::filters::GaussianKernel filter;
+
+    function<void()> test_func = ([&](){
+        filter.process(image->data(), image->width(), image->height(), output.data(), image->spectrum());
+    });
+
+
+
+    #if MULTI_THREADED
+    vector<function<void()>> test_funcs;
+    test_funcs.push_back(test_func);
+    test_funcs.push_back(test_func);
+    test_funcs.push_back(test_func);
+    test_funcs.push_back(test_func);
+    pt.test_threaded(test_funcs);
+    #else
+    pt.test(test_func);
+    #endif
+
+    if (!opts.output_file.empty()) {
+        output.save(opts.output_file.c_str());
+    }
 }
