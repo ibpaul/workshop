7512095868e4d6b5a12a7730471ad467528033de
Paul Lucas <paullucas82@gmail.com>
2022-10-01 08:58:02 -0500

    added some documentation about the optimization flags

--- include/optimize_flags.h
+++ include/optimize_flags.h
@@ -4,7 +4,7 @@
 #define LTS_OPTIMIZE_FLAGS_H
 
 // [OPTIMIZE NOTES]
-//  - Only enable either OPTIMIZE_1 or OPTIMIZE_2. Do not enable both!
+//  - Only enable either OPTIMIZE_1, OPTIMIZE_2 or OPTIMIZE_3. Do not enable multiple ones!
 
 #define OPTIMIZE_1 0
 // OPTIMIZE_1 changes how the kernel weights are accessed in the filter's for loops.
@@ -34,6 +34,9 @@
 
 #define OPTIMIZE_3 1
 // OPTIMIZE_3 converts the kernel weights to a multidimensional array.
+//
+// This optimization configuration is the setup as employed in Gaussian_v1 and provides the
+// same performance level for Gaussian_v2. This is the fast optimization level currently.
 
 
 #endif //LTS_OPTIMIZE_FLAGS_H
