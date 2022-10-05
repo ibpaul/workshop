88457ae350040d5d7b8249700028e676b7b4fba6
Paul Lucas <paullucas82@gmail.com>
2022-10-05 08:22:30 -0500

    bugfix: added in library definition of pi and e to compile on microsoft compiler

--- src/util/statistics.cpp
+++ src/util/statistics.cpp
@@ -1,5 +1,8 @@
 #include "util/statistics.h"
 
+constexpr float pi_f = 3.14159265358979323846f;
+constexpr float e_f = 2.718281828459045;
+
 namespace LTS {
 namespace util {
 
@@ -7,12 +10,12 @@ namespace util {
 float normal_pdf(float x, float sigma, float mu)
 {
     // coefficient
-    const float co = 1.0f / (sigma * sqrtf(2.0f * static_cast<float>(M_PI)));
+    const float co = 1.0f / (sigma * sqrtf(2.0f * static_cast<float>(pi_f)));
 
     // exponent
     float ex = - (1.0f / 2.0f) * powf(((x - mu) / sigma), 2.0f);
 
-    return co * powf(M_E, ex);
+    return co * powf(e_f, ex);
 }
 
 
