b8c0b376f74691a48e348d976f63c34b9e033653
Paul Lucas <paullucas82@gmail.com>
2022-10-07 19:47:21 -0500

    filter continuously runs

--- tools/zephyr_filter/src/main.cpp
+++ tools/zephyr_filter/src/main.cpp
@@ -33,40 +33,34 @@ class A {
 
 int main(void)
 {
-    auto ret = 0;
-	//int ret;
+    auto ret {0};
 
     LTS::filter::KernelFast<float, 3, 3> kernel;
     LTS::filter::load_gaussian(kernel);
 
     auto input = LTS::image::vertical_lines(256, 256, 1);
 
-    auto start = HAL_GetTick();
-    LTS::filter::convolute(kernel, input.get(), 256, 256, 1, output_image);
-    //LTS::filter::convolute(kernel, IMAGE_DATA, 512, 512, 3, output_image);
-    auto end = HAL_GetTick();
-    auto diff = end - start;
-
-    char time_buffer[30];
-    //snprintf(time_buffer, 20, "%d ms\r\n", diff);
-
-    printk("Hello World! %d ms\n", diff);
-
-
-	if (!device_is_ready(led.port)) {
+	/*if (!device_is_ready(led.port)) {
 		return 1;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 1;
-	}
+	}*/
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
+        auto start = HAL_GetTick();
+        LTS::filter::convolute(kernel, input.get(), 256, 256, 1, output_image);
+        auto end = HAL_GetTick();
+        auto diff = end - start;
+
+        printk("%d ms\n", diff);
+
+		/*ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return 1;
 		}
-		k_msleep(SLEEP_TIME_MS);
+		k_msleep(SLEEP_TIME_MS);*/
 	}
 }
