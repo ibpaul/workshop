b3cab0fccb17579cd48652d3fc8f5560ca755bff
Paul Lucas <paullucas82@gmail.com>
2022-10-13 15:56:24 -0500

    restructured play main.cpp

--- tools/play/main.cpp
+++ tools/play/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
 // Hide error message reporting from CImg.
-#define cimg_verbosity 0
+//#define cimg_verbosity 0
 #include "CImg.h"
+#include "util/string.h"
 
 using namespace std;
 using namespace cimg_library;
+
+
+void display_image(const string& filename);
+void play_sound(const string& filename);
+
+
+string get_file_extension(const string& filename)
+{
+    return lts::util::split(filename, '.').back();
+}
 
 
 int main(int argc, char* argv[])
@@ -16,7 +27,27 @@ int main(int argc, char* argv[])
     }
 
     string filename {argv[1]};
+    auto ext = get_file_extension(filename);
 
+    if (ext == "png") {
+        display_image(filename);
+    } else if (ext == "wav") {
+        play_sound(filename);
+    } else {
+        cerr << "Unrecognized file type." << endl;
+        exit(1);
+    }
+}
+
+
+void play_sound(const string& filename)
+{
+
+}
+
+
+void display_image(const string& filename)
+{
     try {
         CImg<unsigned char> image(filename.c_str());
 
@@ -27,6 +58,6 @@ int main(int argc, char* argv[])
         }
     } catch (...) {
         cerr << "Unable to open file '" << filename << "'." << endl;
-        return 1;
+        exit(1);
     }
 }
